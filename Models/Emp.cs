f3b5338658db05633ea780ab52c60ce085a2ace5
Gayatri Bhangle <92245566+GayatriBhangle@users.noreply.github.com>
2026-07-08 10:46:56 +0530

    Add files via upload

---
+++ Models/Emp.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LabPractice.Models;
+
+public partial class Emp
+{
+    public int No { get; set; }
+
+    public string? Name { get; set; }
+
+    public string? Address { get; set; }
+}
